59f5bdb111db7284d7c212903693dbc341f95f33
Claudio Sosa <vicomser.claudio@gmail.com>
2019-06-03 08:28:45 -0300

    first commit

---
+++ platzimusica/src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'platziMusica',
+  apiKey:'77efb3ffe26d8847a62eab651f9fe484',
+  secret: 'e8f89e92c90832d5509e6efb07b85133',
+  registeredTo: 'claudiososa'
+}
+
+// Application name
+// API key 	77efb3ffe26d8847a62eab651f9fe484
+// Shared secret 	e8f89e92c90832d5509e6efb07b85133
+// Registered to 	claudiososa
